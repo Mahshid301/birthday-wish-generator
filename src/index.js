edb66b9538323da401e721e34a99862d84f98908
Mahshid301 <mahshid.m970@gmail.com>
2025-04-22 19:40:22 +0330

    api integrated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-function generateWish(event) {
-  event.preventDefault();
+function displayWish(response) {
   new Typewriter("#wish", {
-    strings: "Happy Birthday! May your day be filled with so much joy!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generateWish(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instruction");
+  let apiKey = "t9d32fe46026d08obe2316b28af5bf2b";
+  let prompt = `please generate a birthday wish including ${instructionInput.value}`;
+  let context = "please generate a lovely birthday wish no longer than 3 lines";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayWish);
 }
 
 let wishFormElement = document.querySelector("#wish-generator-form");
